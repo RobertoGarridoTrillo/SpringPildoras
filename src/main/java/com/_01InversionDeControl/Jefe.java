22b865212fdd6c95be81c43477e4d260b8c9cf55
RobertoGarridoTrillo <robegt72@hotmail.com>
2020-06-20 19:33:39 +0200

    Este es la creacion del paquete

---
+++ src/main/java/com/_01InversionDeControl/Jefe.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com._01InversionDeControl;
+
+/**
+ *
+ * @author Roberto Garrido Trillo 
+ */
+public class Jefe {
+
+}
---
+++ src/main/java/com/_01InversionDeControl/UsoEmpleado.java
@@ -0,0 +1,16 @@
+package com._01InversionDeControl;
+
+/**
+ *
+ * @author Roberto Garrido Trillo
+ */
+public class UsoEmpleado {
+
+   public static void main (String[] args) {
+      // creacion de objetos de tipo empleado
+      Jefe Empleado1 = new Jefe();
+      // uso de los objetos creados
+      System.out.println(Empleado1.getTareas());
+
+   }
+}
